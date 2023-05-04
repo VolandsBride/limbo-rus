8e68ac36d48d1a2363276bd6e2f077f0aa986ad6
VolandsBride <volands.bride@gmail.com>
2023-05-05 00:28:55 +0300

    JS audio player added - yay!!!

--- js/main.js
+++ js/main.js
@@ -1,58 +1,179 @@
-// $(function () {
-  
-// });
-
-
-sound.play();
-
-const musicContainer = document.querySelector('.music-container');
-const playBtn = document.querySelector('#play');
-const prevBtn = document.querySelector('#prev');
-const nextBtn = document.querySelector('#next');
+const playBtn = document.querySelector('#mainPlayBtn');
 const audio = document.querySelector('#audio');
+const btnPrev = document.querySelector('#btnPrev');
+const btnNext = document.querySelector('#btnNext');
+const trackTitle = document.querySelector('.track-title');
+const slider = document.querySelector('.slider');
+const thumb = document.querySelector('.slider-thumb');
 const progress = document.querySelector('.progress');
-const progressContainer = document.querySelector('.progress-container');
-const songTitle = document.querySelector('#song-title');
+const time = document.querySelector('.time');
+const fullTime = document.querySelector('.fulltime');
+const volumeSlider = document.querySelector('.volume-slider .slider');
+const volumeProgress = document.querySelector('.volume-slider .progress');
+const volumeIcon = document.querySelector('.volume-icon');
 
-// Song titles
-const songs = ['limbo - taxi (sodade)', 'limbo - sodade (sodade)', 'limbo - love (sodade)'];
+let trackPlaying = false;
+let volumeMuted = false;
+let trackId = 0;
 
-// Keep track of songs 
-let songIndex = 0;
+const tracks = [
+  'taxi (sodade)',
+  'sodade (sodade)',
+  'love (sodade)',
+];
 
-// Initially load song info DOM
-loadSong(songs[songIndex]);
+playBtn.addEventListener('click', playTrack);
 
-// Update the song detail
-function loadSong(song) {
-  songTitle.innerText = song;
-  audio.src = '/music/${song}.mp3';
-}
-
-function playSong() {
-  musicContainer.classList.add('play')
-  playBtn.querySelector('i.fas').classList.remove('fa-play');
-  playBtn.querySelector('i.fas').classList.add('fa-pause');
-
-  audio.play()
-}
-
-function pauseSong() {
-  musicContainer.classList.remove('play')
-  playBtn.querySelector('i.fas').classList.add('fa-play');
-  playBtn.querySelector('i.fas').classList.remove('fa-pause');
-
-  audio.pause()
-
-}
-
-//Event Listeners 
-playBtn.addEventListener('click', () => {
-  const isPlaying = musicContainer.classList.contains('play')
-
-  if (isPlaying) {
-    pauseSong()
+function playTrack() {
+  if (trackPlaying === false) {
+    audio.play();
+    playBtn.innerHTML = `
+    <span class="material-symbols-outlined">
+      pause
+    </span>
+    `;
+    trackPlaying = true;
   } else {
-    playSong()
+    audio.pause();
+    playBtn.innerHTML = `
+    <span class="material-symbols-outlined">
+      play_arrow
+    </span>`;
+    trackPlaying = false;
   }
-})
+}
+
+function switchTrack() {
+  if (trackPlaying === true) {
+    audio.play();
+  }
+}
+
+const trackSrc = 'audio/' + tracks[trackId] + ".mp3";
+
+function loadTrack() {
+  audio.src = 'audio/' + tracks[trackId] + ".mp3";
+  audio.load();
+  trackTitle.innerHTML = tracks[trackId];
+  progress.style.width = 0;
+  thumb.style.left = 0;
+
+  audio.addEventListener('loadeddata', () => {
+    setTime(fullTime, audio.duration);
+    slider.setAttribute("max", audio.duration);
+  });
+}
+
+loadTrack();
+btnPrev.addEventListener('click', () => {
+  trackId--;
+  if(trackId < 0) {
+    trackId = tracks.length - 1;
+  }
+  loadTrack();
+  switchTrack();
+});
+
+btnNext.addEventListener('click', nextTrack);
+
+function nextTrack() {
+  trackId++;
+  if(trackId > tracks.length - 1) {
+    trackId = 0;
+  }
+  loadTrack();
+  switchTrack();
+}
+
+audio.addEventListener('ended', nextTrack);
+
+function setTime(output, input) {
+  const minutes = Math.floor(input / 60);
+  const seconds = Math.floor(input % 60);
+
+  if(seconds < 10) {
+    output.innerHTML = minutes + ":0" + seconds;
+  } else {
+    output.innerHTML = minutes + ":" + seconds;
+  }
+}
+
+setTime(fullTime, audio.duration);
+
+audio.addEventListener('timeupdate', () => {
+  const currentAudioTime = Math.floor(audio.currentTime);
+  const timePercentage = (currentAudioTime / audio.duration) * 100 + "%";
+  setTime(time, currentAudioTime);
+  progress.style.width = timePercentage;
+  thumb.style.left = timePercentage;
+});
+
+function customSlider() {
+  const val = (slider.value / audio.duration) * 100 + "%";
+  progress.style.width = val;
+  thumb.style.left = val;
+
+  setTime(time, slider.value);
+  audio.currentTime = slider.value;
+}
+
+customSlider();
+slider.addEventListener("input", customSlider);
+
+
+let val;
+
+function customVolumeSlider() {
+  const maxVal = volumeSlider.getAttribute("max");
+  val = (volumeSlider.value / maxVal) * 100 + "%";
+  volumeProgress.style.width = val;
+  audio.volume = volumeSlider.value / 100;
+  if(audio.volume > 0.5) {
+    volumeIcon.innerHTML = `
+    <span class="material-symbols-outlined">
+      volume_up
+    </span>
+    `;
+  } else if (audio.volume === 0) {
+    volumeIcon.innerHTML = `
+    <span class="material-symbols-outlined">
+      volume_off
+    </span>
+    `;
+  } else {
+     volumeIcon.innerHTML = `
+    <span class="material-symbols-outlined">
+      volume_down
+    </span>
+    `;
+  }
+}
+
+customVolumeSlider();
+
+volumeSlider.addEventListener(
+  "input", customVolumeSlider
+);
+
+
+volumeIcon.addEventListener('click', () => {
+  if (volumeMuted === false) {
+    volumeIcon.innerHTML = `
+    <span class="material-symbols-outlined">
+      volume_off
+    </span>
+    `;
+    audio.volume = 0;
+    volumeProgress.style.width = 0;
+    volumeMuted = true;
+  } else {
+    volumeIcon.innerHTML = `
+    <span class="material-symbols-outlined">
+      volume_down
+    </span>
+    `;
+    audio.volume = 0.5;
+    volumeProgress.style.width = val;
+    volumeMuted = false;
+  }
+});
